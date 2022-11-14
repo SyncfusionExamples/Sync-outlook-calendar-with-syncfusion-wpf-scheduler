691e04c32836ad905f91052d32e840cfdd3c333b
Jeyasri Murugan <jeyasrim@syncfusion.com>
2022-11-14 10:20:00 +0530

    Sample added

---
+++ SyncOutlookCalendar/SyncOutlookCalendar/App.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.Identity.Client;
+
+
+namespace SyncOutlookCalendar
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        //public static UIParent UiParent;
+        public static IPublicClientApplication ClientApplication;
+        //You need to replace your Application ID
+        public static string ClientID = "8d0bd3de-1b47-4844-9bd0-61744355cc3b";
+        public static string[] Scopes = { "User.Read", "Calendars.Read", "Calendars.ReadWrite" };
+        private const string Tenant = "77f1fe12-b049-4919-8c50-9fb41e5bb63b";
+        private const string Authority = "https://login.microsoftonline.com/" + Tenant;
+
+        public App()
+        {
+            ClientApplication = PublicClientApplicationBuilder.Create(ClientID)
+            .WithAuthority(AzureCloudInstance.AzurePublic, Tenant)
+            .WithDefaultRedirectUri()
+            .Build();
+        }
+
+
+    }
+}
---
+++ SyncOutlookCalendar/SyncOutlookCalendar/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.Graph;
+using Microsoft.Graph.Models;
+using Microsoft.Identity.Client;
+using System;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows;
+using static System.Formats.Asn1.AsnWriter;
+using System.Net.Http;
+using Microsoft.Graph.Models.ExternalConnectors;
+using Microsoft.Graph.Models.Security;
+using System.Collections.Generic;
+using System.Xml.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Text;
+
+namespace SyncOutlookCalendar
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Authenticate();
+        }
+
+        static async Task Authenticate()
+        {
+            var accounts = await App.ClientApplication.GetAccountsAsync();
+            AuthenticationResult tokenRequest;
+            if (accounts.Count() > 0)
+            {
+                tokenRequest = await App.ClientApplication.AcquireTokenSilent(App.Scopes, accounts.FirstOrDefault())
+                    .ExecuteAsync();
+            }
+            else
+            {
+                tokenRequest = await App.ClientApplication.AcquireTokenInteractive(App.Scopes).ExecuteAsync();
+
+            }
+
+            HttpClient client = new HttpClient();
+            var request = new System.Net.Http.HttpRequestMessage(System.Net.Http.HttpMethod.Get, "https://graph.microsoft.com/v1.0");
+
+            //Add the token in Authorization header
+            request.Headers.Authorization = new AuthenticationHeaderValue("bearer", tokenRequest?.AccessToken);
+          
+            //var response = await client.SendAsync(request);
+
+            //HttpResponseMessage getEvents = await client.GetAsync("https://graph.microsoft.com/beta/me/calendar/events");
+            //// if (getEvents.IsSuccessStatusCode)
+            //{
+            //    string json = await getEvents.Content.ReadAsStringAsync();
+            //    var result1 = JsonConvert.DeserializeObject(json);
+
+            //    // foreach (JProperty child in result.Properties().Where(p => !p.Name.StartsWith("@")))
+            //} 
+
+            Uri addEventUri = new Uri("https://graph.microsoft.com/beta/me/calendar/events");
+
+            Event newEvent = new Event()
+            {
+                Subject = "Sample Data 1",
+                Start = new DateTimeTimeZone() { DateTime = DateTime.Now.ToString(), TimeZone = "Eastern Standard Time" },
+                End = new DateTimeTimeZone() { DateTime =  DateTime.Now.AddHours(1).ToString(), TimeZone = "Eastern Standard Time" },
+            };
+            HttpContent addEventContent = new StringContent(JsonConvert.SerializeObject(newEvent), Encoding.UTF8, "application/json");
+            HttpResponseMessage addEventResponse = await client.PostAsync(addEventUri, addEventContent);
+
+            if (addEventResponse.IsSuccessStatusCode)
+            {
+                Console.WriteLine("Event has been added successfully!");
+            }
+        }
+    }
+}
